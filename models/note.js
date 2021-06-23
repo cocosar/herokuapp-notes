2e95278fb76cdb1f76f7cd78c9ba79ab6e360b6c
Juan José Sarsotti <cocosarsotti@gmail.com>
2021-06-22 22:00:37 -0300

    uibuild

--- models/note.js
+++ models/note.js
@@ -1,36 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-  const noteSchema = new mongoose.Schema({
+const noteSchema = new mongoose.Schema({
     content: {
-      type: String,
-      minLength: 5,
-      required: true
+        type: String,
+        required: true,
+        minlength: 5
     },
-    date: { 
-      type: Date,
-      required: true
+    date: {
+        type: Date,
+        required: true,
     },
-    important: Boolean
-  })
+    important: Boolean,
+})
 
 noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
